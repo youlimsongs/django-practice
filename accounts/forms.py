5b2e987b9b29ffca2f28dfe6d9d011504001421f
youlimsongs <giuhki@naver.com>
2023-07-04 15:58:21 +0900

    create User Form

---
+++ accounts/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import User
+
+class SIgnUpForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=255,
+        help_text='Enter a valid email address'
+    )
+    
+    nickname = forms.CharField(
+        max_length=255,
+        help_text='Enter a valid nickname',
+        required=True
+    )
+    
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        help_text='Your password must contain at least 8 characters',
+    )
+    
+    passwowrd2 = forms.CharField(
+        label='Password Confirmation',
+        widget=forms.PasswordInput(),
+        help_text='Enter the same password as before, for verification.',
+    )
+    
+    class Meta:
+        model = User
+        fields = {'email', 'nickname', 'password1', 'password2'}
+        
+class SignInForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = {'email', 'password'}
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from accounts.forms import SIgnUpForm
+
+def signup(request):
+    if request.method == 'GET':
+        form = SIgnUpForm()
+    elif request.method =='POST':
+        form = SIgnUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('signin')
+    return render(request, 'accounts/signup.html', {'form':form})
+    
+def signin(request):
+    if request.method == 'GET':
+        form = SIgnUpForm()
+    elif request.method =='POST':
+        form = SIgnUpForm(request, data = request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, email=email, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('todo_list')
+            else:
+                messages.error(request)
+    return render(request, 'accounts/signup.html', {'form':form})
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
+    
